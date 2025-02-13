7e83435a7963be923fb087f8bf6ee92ea2a42021
rakeshkidecha <kidecharakesh2002@gmail.com>
2025-02-13 21:16:04 +0530

    Forget Password, chnage password and validation add

--- controller/UserCtl.js
+++ controller/UserCtl.js
@@ -1,6 +1,9 @@
 const User = require("../models/UserModel");
 const Cryptr = require('cryptr');
 const cryptr = new Cryptr('SecretKeydfhkdi8df');
+const bcrypt = require('bcrypt');
+const {validationResult} = require('express-validator');
+const nodemailer = require("nodemailer");
 
 module.exports.login = async(req,res)=>{
     try {
@@ -20,7 +23,7 @@ module.exports.checkLogin = async(req,res)=>{
         const existUser = await User.findOne({email:req.body.email});
 
         if(existUser){
-            if(existUser.password===req.body.password){
+            if(await bcrypt.compare(req.body.password,existUser.password)){
                 const encryptUserData = cryptr.encrypt(JSON.stringify(existUser))
                 res.cookie('user',encryptUserData)
                 return res.redirect('/home');
@@ -43,27 +46,31 @@ module.exports.signUp = async(req,res)=>{
         if(req.cookies.user){
             return res.redirect('/home');
         }else{
-            return res.render('users/signUp');
+            return res.render('users/signUp',{
+                errors:null,
+                oldValue:null
+            });
         }
     } catch (err) {
-        console.log("SOmething Wrong",err)
-        return res.redirect('back')
+        console.log("Something Wrong",err)
+        return res.redirect('/signUp')
     }
 }
 
 module.exports.insertUser = async(req,res)=>{
     try {
 
-        const isExistEmail = await User.find({email:req.body.email}).countDocuments();
-        
-        if(isExistEmail != 0){
-            console.log("Email is already Exist, please try another email....");
-            return res.redirect('back');
-        }
-        
-        if(req.body.password != req.body.confirmPassword){
-            console.log("Password and confirm password not match..");
-            return res.redirect('back');
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors.mapped());
+            if(req.cookies.user){
+                return res.redirect('/home');
+            }else{
+                return res.render('users/signUp',{
+                    errors:errors.mapped(),
+                    oldValue:req.body
+                });
+            }
         }
 
         var imagePath = '';
@@ -73,6 +80,7 @@ module.exports.insertUser = async(req,res)=>{
 
         req.body.profile_image = imagePath;
         req.body.name = req.body.fName+' '+req.body.lName;
+        req.body.password = await bcrypt.hash(req.body.password,10);
 
         const addedUser = await User.create(req.body);
 
@@ -83,13 +91,13 @@ module.exports.insertUser = async(req,res)=>{
             return res.redirect('/home');
         }else{
             console.log("Account Create Failed");
-            return res.redirect('back')
+            return res.redirect('/signUp')
         }
 
 
     } catch (err) {
         console.log("Something Wrong",err)
-        return res.redirect('back')
+        return res.redirect('/signUp')
     }
 }
 
@@ -100,5 +108,168 @@ module.exports.logout = async(req,res)=>{
     } catch (err) {
         console.log("Something Wrong",err)
         return res.redirect('back')
+    }
+};
+
+module.exports.chnagePassword = async(req,res)=>{
+    try {
+        const user = JSON.parse(cryptr.decrypt(req.cookies.user))
+        if(!user){
+            return res.redirect('/')
+        }
+        return res.render('users/changePassword',{user,errors:null,oldValue:null});
+
+    } catch (err) {
+        console.log("Something Wrong",err)
+        return res.redirect('back')
+    }
+};
+
+module.exports.checkChangePassword = async(req,res)=>{
+    try {
+        const errors = validationResult(req);
+        const user = JSON.parse(cryptr.decrypt(req.cookies.user));
+        if(!errors.isEmpty()){
+            return res.render('users/changePassword',{user,errors:errors.mapped(),oldValue:req.body})
+        };
+        const isExistUser = await User.findById(user._id);
+        if(isExistUser){
+            const newPass = await bcrypt.hash(req.body.newPassword,10);
+            const updatePass = await User.findByIdAndUpdate(isExistUser._id,{password:newPass});
+            if(updatePass){
+                console.log("Password Change successfully");
+                res.clearCookie('user');
+                return res.redirect('/')
+            }else{
+                console.log("Password not Updated");
+                return res.redirect('/changePassword')
+            }
+        }else{
+            console.log("User not found")
+            return res.redirect('/changePassword')
+        }
+
+    } catch (err) {
+        console.log("Something Wrong",err)
+        return res.redirect('/changePassword')
+    }
+}
+
+module.exports.verifyEmail = async(req,res)=>{
+    try {
+        const isExistUser = await User.findOne({email:req.body.email});
+        if(isExistUser){
+
+            const OTP = Math.ceil(Math.random()*10000);
+
+            const transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 587,
+                secure: false, // true for port 465, false for other ports
+                auth: {
+                  user: "workrakesh04@gmail.com",
+                  pass: "onmmfbnogttrtzui",
+                },
+            });
+
+            const info = await transporter.sendMail({
+                from: 'workrakesh04@gmail.com', // sender address
+                to: isExistUser.email, // list of receivers
+                subject: "Verify Otp", // plain text body
+                html: `<p>your OTP For forget password is <b>${OTP}</b></p>`, // html body
+            });
+        
+            console.log("Message sent: %s", info.messageId);
+
+            res.cookie('otp',cryptr.encrypt(JSON.stringify(OTP)),{maxAge:30*1000});
+            res.cookie('email',cryptr.encrypt(JSON.stringify(isExistUser.email)));
+
+            return res.redirect('/checkOtp');
+
+        }else{
+            console.log("User not Found");
+            return res.redirect('back');
+        }
+
+    } catch (err) {
+        console.log("Something Wrong",err)
+        return res.redirect('back')
+    }
+};
+
+module.exports.checkOtp = async(req,res)=>{
+    try {
+        let isOtp = false;
+        if(req.cookies.otp){
+            isOtp = true;
+        }
+        const email = JSON.parse(cryptr.decrypt(req.cookies.email));
+        return res.render('users/checkOtp',{email,isOtp});
+    } catch (err) {
+        console.log("Something Wrong",err)
+        return res.redirect('back');
+    }
+};
+
+module.exports.verifyOtp = async(req,res)=>{
+    try {
+        const otp = JSON.parse(cryptr.decrypt(req.cookies.otp));
+
+        if(otp == req.body.otp){
+            res.clearCookie('otp');
+            return res.redirect('/forgetPassword');
+        }else{
+            console.log("Invalid Otp");
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.log("Something Wrong",err)
+        return res.redirect('back');
+    }
+};
+
+module.exports.forgetPassword  = async(req,res)=>{
+    try {
+        return res.render('users/forgetPassword',{
+            errors : null,
+            oldValue :null
+        })
+    } catch (err) {
+        console.log(err);
+        return res.redirect('back');
+    }
+}
+
+module.exports.checkPassword = async(req,res)=>{
+    try {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.render('users/forgetPassword',{
+                errors : errors.mapped(),
+                oldValue :req.body
+            });  
+        }
+
+        const email = JSON.parse(cryptr.decrypt(req.cookies.email));
+        const userData = await User.findOne({email:email});
+        if(userData){
+            if(req.body.newPassword == req.body.confirmPassword){
+                const newPass = await bcrypt.hash(req.body.newPassword,10);
+                const forgetUserPass = await User.findByIdAndUpdate(userData._id,{password:newPass});
+                res.clearCookie('email');
+                console.log("Password forgeted");
+                return res.redirect('/');
+            }else{
+                console.log("New And Confirm password are not Match");
+                return res.redirect('/forgetPassword');
+            }
+        }else{
+            console.log("User not found");
+            return res.redirect('/forgetPassword')
+        }
+    } catch (err) {
+        console.log("Something Wrong",err)
+        return res.redirect('/forgetPassword');
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const UserCtl = require('../controller/UserCtl');
 const User = require('../models/UserModel');
+const {check} = require('express-validator');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 // user login-
 router.get('/',UserCtl.login);
@@ -11,10 +13,73 @@ router.post('/checkLogin',UserCtl.checkLogin);
 // user sign up 
 router.get('/signUp',UserCtl.signUp);
 
-router.post('/insertUser',User.uploadProfileImage,UserCtl.insertUser);
+router.post('/insertUser',User.uploadProfileImage,[
+    check('fName').notEmpty().withMessage('First Name is required').isLength({min:2}).withMessage("First Name is Require Minmum 2 Carector of length"),
+    check('lName').notEmpty().withMessage('Last Name is required').isLength({min:2}).withMessage("Last Name is Require Minmum 2 Carector of length"),
+    check('email').notEmpty().withMessage('Email is Required').isEmail().withMessage("Invalid Imail").custom(async value=>{
+        const isExistEmail = await User.findOne({email:value});
+        if(isExistEmail){
+            throw new Error('This Email is Already Exist');
+        }
+    }),
+    check('password').notEmpty().withMessage("Password Are Required").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i").withMessage('Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long'),
+    check('confirmPassword').notEmpty().withMessage("Confirm Password Are Required").custom(async (value,{req})=>{
+        if(value != req.body.password){
+            throw new Error('Password and Confirm Password are not match');
+        }
+    })
+],UserCtl.insertUser);
 
 // logout 
 router.get('/logout',UserCtl.logout);
+
+// chnage password 
+router.get('/chnagePassword',UserCtl.chnagePassword);
+router.post('/checkChangePassword',[
+    check('oldPassword').notEmpty().withMessage("Old Password is Required").custom(async (value,{req})=>{
+        const isExistUser = await User.findById(req.body.id)
+        if(!await bcrypt.compare(value,isExistUser.password)){
+            throw new Error('Old Password is not match');
+        }
+    }),
+    check('newPassword').notEmpty().withMessage("New Password is required").custom(async (value,{req})=>{
+        if(value == req.body.oldPassword){
+            throw new Error('Old and New Password are same');
+        }
+    }),
+    check('confirmPassword').notEmpty().withMessage("Confirm Password is required").custom(async (value,{req})=>{
+        if(value != req.body.newPassword){
+            throw new Error('New and Confirm Password are not Match');
+        }
+    }),
+],UserCtl.checkChangePassword);
+
+//forget password
+router.get('/checkEmail',async (req,res)=>{
+    try {
+        return res.render('users/checkEmail');
+    } catch (err) {
+        console.log(err);
+        return res.redirect('back');
+    }
+});
+
+router.post('/verifyEmail',UserCtl.verifyEmail);
+
+router.get('/checkOtp',UserCtl.checkOtp);
+
+router.post('/verifyOtp',UserCtl.verifyOtp);
+
+router.get('/forgetPassword',UserCtl.forgetPassword);
+
+router.post('/checkPassword',[
+    check('newPassword').notEmpty().withMessage("New Password is Required"),
+    check('confirmPassword').notEmpty().withMessage("Confirm Password is Required").custom(async (value,{req})=>{
+        if(value != req.body.newPassword){
+            throw new Error('New and Confirm Password are not Match');
+        }
+    })
+],UserCtl.checkPassword);
 
 
 module.exports = router;
